d93e176996edcf403d8cd5bb0584819aa00224ad
Игорь Голяк <golyak.03@inbox.ru>
2021-05-17 14:12:37 +0700

    17

---
+++ 17.05.2021/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._05._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.41
+            Console.WriteLine("Ввидете число X");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = 0;
+            if (x>=0.2 && x <= 0.9)
+            {
+                y = Math.Sin(x);
+            }
+            else
+            {
+                y = 1;
+            }
+            Console.WriteLine("y=" + y);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*Составить программу "КАЛЬКУЛЯТОР", которая после ввода
+            //* двух чисел и одного из чисел 1 2 3 4(которые соотвуствуют +,-,*,/)
+            //* произойдет вычисления,а результат выдаст на экран.//
+
+            Console.WriteLine("Введите первое число");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третие число");
+            double z = Convert.ToDouble(Console.ReadLine());
+            double c = 0;
+            switch (z)
+            {
+                case 1: c = x + y;break;
+                case 2: c = x - y;break;
+                case 3: c = x * y; break;
+                case 4: c = x / y; break;
+
+                /*дописать*/
+                default: Console.WriteLine("Ошибка выбора оператора");break;
+            }
+            /*Вывести ответ*/
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
